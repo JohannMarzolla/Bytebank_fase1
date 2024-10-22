f3d83d65a791c409406011c42ba67f74d68aa20a
Lucas Rainoldo Janzen <48385544+lucasjanzen@users.noreply.github.com>
2024-10-21 21:31:22 -0300

    Header não logado responsivo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "./LoginForm";
 
 export default async function Page() {
   return (
-    <div className="flex justify-center w-full h-full">
+    <div className="flex justify-center w-full h-full sm:p-10 max-sm:py-5">
       <LoginForm />
     </div>
   );
--- src/app/nova-conta/page.tsx
+++ src/app/nova-conta/page.tsx
@@ -2,7 +2,7 @@ import NovaContaForm from "./NovaContaForm";
 
 export default async function Page() {
   return (
-    <div className="flex justify-center w-full h-full">
+    <div className="flex justify-center w-full sm:p-10 max-sm:py-5">
       <NovaContaForm />
     </div>
   );

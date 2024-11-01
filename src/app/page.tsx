16debd48bd524e01db58e79d43d7dd0f00162d71
Lucas Rainoldo Janzen <48385544+lucasjanzen@users.noreply.github.com>
2024-10-31 21:00:57 -0300

    ajuste css home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,13 @@ export default async function Home() {
       <div className="flex flex-col justify-between h-full w-full overflow-x-hidden overflow-y-scroll">
         <div className="flex flex-col">
           <div className="bg-bottom  md:bg-cover sm:bg-contain md:min-h-screen">
-            <div className="flex flex-col items-center lg:flex-row justify-between max-w-6xl mx-auto w-full mt-10">
+            <div className="flex flex-col items-center lg:flex-row justify-between max-w-6xl mx-auto w-full mt-10 px-4">
               <h1 className="text-2xl font-bold text-black text-center lg:text-left mb-4 lg:mb-0 lg:w-1/2">
                 Experimente mais liberdade no <br /> controle da sua vida financeira. <br /> Crie sua conta com a gente!
               </h1>
               <img src="/ilustracao-banner.svg" alt="" className="w-full lg:w-1/2 max-w-xs lg:max-w-none mx-auto" />
             </div>
-            <div className="md:hidden flex justify-center mt-3 gap-6">
+            <div className="md:hidden flex justify-center mt-3 gap-6 flex-wrap">
               <Link href="/nova-conta">
                 <Button color="black" text="Abrir minha conta" />
               </Link>
@@ -24,7 +24,7 @@ export default async function Home() {
                 <Button color="transparent" text="Já tenho conta" outlined={true} />
               </Link>
             </div>
-            <div className="flex flex-wrap justify-between max-w-6xl mx-auto w-full mt-10 mb-10">
+            <div className="flex flex-wrap justify-between max-w-6xl mx-auto w-full mt-10 mb-10 px-10 gap-3">
               <div className="flex flex-col items-center w-full sm:w-1/2 lg:w-1/4 p-4">
                 <img src="/icon-gift.svg" alt="Conta e cartão gratuitos" className="mb-2" />
                 <h2 className="text-lg font-bold text-fiap-green font-inter text-[20px] flex items-center mb-1">

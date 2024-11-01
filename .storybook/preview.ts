d0d6eaece426569a98e7eb5382ba6d7b849bfba2
Lucas Rainoldo Janzen <48385544+lucasjanzen@users.noreply.github.com>
2024-11-01 20:26:12 -0300

    StoryBook com tailwind

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import "../src/app/globals.css"; // Supondo que o Tailwind esteja configurado nesse arquivo
 
 const preview: Preview = {
   parameters: {
